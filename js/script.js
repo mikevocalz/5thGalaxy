b426377288300902891b97d9b9c86141a9f0a366
mikevocalz <mikevocalz@aol.com>
2017-11-11 14:10:28 -0500

    5th Galaxy 1st Commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".button-collapse").sideNav({
+    menuWidth: 200,
+    closeOnClick: true,
+    draggable: true
+  });
+  $(".parallax").parallax();
+  $(".carousel.carousel-slider").carousel({ fullWidth: true });
+  $(".materialboxed").materialbox();
+  $(".slider").slider();
+});
